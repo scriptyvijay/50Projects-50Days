3ea95794513a04df8b6eb487d9bd858a49db5625
Vijay Choudhary <scriptyvijay@gmail.com>
2023-03-23 12:41:25 +0530

    completed 1st project

--- 1-Expanding-Cards/script.js
+++ 1-Expanding-Cards/script.js
@@ -1,1 +1,14 @@
+let panels = document.querySelectorAll(".panel");
 
+panels.forEach((panel) => {
+	panel.addEventListener("click", () => {
+		removeClasses();
+		panel.classList.add("active");
+	});
+});
+
+function removeClasses() {
+	panels.forEach((panel) => {
+		panel.classList.remove("active");
+	});
+}
